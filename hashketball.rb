4b04792a7b3cac4cc1250b368bb72461a1ac1912
Shakore McGill <mcgills2093@gmail.com>
2020-05-14 16:20:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,12 +67,12 @@ def game_hash
         player_name: "Jeff Adrien"
         number: 4,
         shoe: 18,
-        points =>"10,
-        rebounds =>"1", 
-        assists =>"1",
-        steals =>"2",
-        blocks =>"7",
-        slam_dunks =>"2"
+        points: 10,
+        rebounds: 1, 
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
       },
       {
         player_name =>"Bismack Biyombo"
