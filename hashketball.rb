58aaa85a45e61484f61b39f09e94963d2103d27b
Shakore McGill <mcgills2093@gmail.com>
2020-05-14 16:19:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,10 +64,10 @@ def game_hash
       team_name => "Charlotte Hornets", 
       colors => ["Turquoise", "Purple"], 
       players => [{
-        player_name =>"Jeff Adrien"
-        number =>"4",
-        shoe =>"18",
-        points =>"10",
+        player_name: "Jeff Adrien"
+        number: 4,
+        shoe: 18,
+        points =>"10,
         rebounds =>"1", 
         assists =>"1",
         steals =>"2",
