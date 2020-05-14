6c4ce0b4bda4207c75dad6ccf0ef553cc9ccbee7
Shakore McGill <mcgills2093@gmail.com>
2020-05-14 16:17:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,11 +28,11 @@ def game_hash
       {
         player_name =>"Brook Lopez"
         number: 11,
-        shoe 17,
-        points 17,
-        rebounds 19, 
-        assists 10,
-        steals 3,
+        shoe: 17,
+        points: 17,
+        rebounds: 19, 
+        assists: 10,
+        steals: 3,
         blocks: 1,
         slam_dunks: 15
       },
