44c85ca44e45934074f8cf7e19ca30583b353ffc
Shakore McGill <mcgills2093@gmail.com>
2020-05-14 16:17:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,14 +27,14 @@ def game_hash
       },
       {
         player_name =>"Brook Lopez"
-        number =>"11",
-        shoe =>"17",
-        points =>"17",
-        rebounds =>"19", 
-        assists =>"10",
-        steals =>"3",
-        blocks =>"1",
-        slam_dunks =>"15"
+        number: 11,
+        shoe 17,
+        points 17,
+        rebounds 19, 
+        assists 10,
+        steals 3,
+        blocks: 1,
+        slam_dunks: 15
       },
       {
         player_name =>"Mason Plumlee"
